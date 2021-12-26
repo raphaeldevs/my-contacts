5a61ea570f4dea3841aaded4d30755281c5b810b
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-26 03:16:21 -0300

    feat[WIP]: handle submit form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,28 +1,62 @@
 import PropTypes from 'prop-types'
 
+import { useState } from 'react'
+
 import FormGroup from '../FormGroup'
 import Input from '../Input'
 import Button from '../Button'
 
-import { ButtonContainer, Form } from './styles'
+import { ButtonContainer } from './styles'
 
 function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [category, setCategory] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    console.log({ name, email, phone, category })
+  }
+
   return (
-    <Form>
+    <form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input name="name" placeholder="Nome" />
-      </FormGroup>
-
-      <FormGroup error="O formato do e-mail é inválido">
-        <Input name="email" placeholder="E-mail" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input name="phone" placeholder="Telefone" />
+        <Input
+          name="name"
+          placeholder="Nome"
+          onChange={event => setName(event.target.value)}
+          value={name}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input as="select">
+        <Input
+          name="email"
+          placeholder="E-mail"
+          onChange={event => setEmail(event.target.value)}
+          value={email}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          name="phone"
+          placeholder="Telefone"
+          onChange={event => setPhone(event.target.value)}
+          value={phone}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          as="select"
+          name="category"
+          onChange={event => setCategory(event.target.value)}
+          value={category}
+        >
+          <option value="">Selecione uma categoria</option>
           <option value="work">Work</option>
         </Input>
       </FormGroup>
@@ -30,7 +64,7 @@ function ContactForm({ buttonLabel }) {
       <ButtonContainer>
         <Button type="submit">{buttonLabel}</Button>
       </ButtonContainer>
-    </Form>
+    </form>
   )
 }
 
--- src/components/ContactForm/styles.js
+++ src/components/ContactForm/styles.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components'
 
-export const Form = styled.form``
-
 export const ButtonContainer = styled.div`
   margin-top: 1.5rem;
 
